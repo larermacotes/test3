c3bc79e56bbfc9d6bb2ab9406abce14cf64aef46
larermacotes <larermacotes@protonmail.com>
2023-02-20 12:40:39 +0100

    Adde features

--- first.py
+++ first.py
@@ -1,7 +1,11 @@
 #This is the first line
 #This is the sedond line
-
+#this is a new line in branch "New features"
 #This is the third line
 #This is the fourth line
 
 print("Alt i orden")
+
+#This are new things!
+#Absolutely! This is for real!
+
